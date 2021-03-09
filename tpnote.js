a8297d5ec20c887f7d634d309b3822d90e37728b
Justine <ribasjusti@eisti.eu>
2021-03-09 23:08:19 +0100

    Possibilité d'ajouter ou de retirer des articles dans "choix"

--- tpnote.js
+++ tpnote.js
@@ -1,1 +1,68 @@
+function ajouterOnClick(magasin, fonction) {
+  var images = document.getElementById(magasin).children;
+  for (var i = 0 ; i < images.length ; i++){
+    images[i].setAttribute("onclick", fonction);
+  }
+}
 
+function ajouter(image) {
+  var nvImage = image.cloneNode(true);
+  var idDest;
+  var idInit;
+  nvImage.setAttribute("onclick", "retirer(this)");
+  switch(nvImage.getAttribute("id")) {
+    case "imageChap" :
+      idDest = "choixChapeau";
+      idInit = "magasinChapeau";
+      break;
+    case "imageHaut" :
+      idDest = "choixHaut";
+      idInit = "magasinVetementHaut";
+      break;
+    case "imageBas" :
+      idDest = "choixBas";
+      idInit = "magasinVetementBas";
+      break;
+    case "imageChaussure" :
+      idDest = "choixChaussure";
+      idInit = "magasinChaussure";
+      break;
+    default :
+      console.log("Error id image");
+      break;
+  }
+  document.getElementById(idDest).appendChild(nvImage);
+  document.getElementById(idInit).removeChild(image);
+  ajouterOnClick(idInit, "");
+}
+
+function retirer(image) {
+  var nvImage = image.cloneNode(true);
+  var idDest;
+  var idInit;
+  nvImage.setAttribute("onclick", "ajouter(this)");
+  switch(nvImage.getAttribute("id")) {
+    case "imageChap" :
+      idInit = "choixChapeau";
+      idDest = "magasinChapeau";
+      break;
+    case "imageHaut" :
+      idInit = "choixHaut";
+      idDest = "magasinVetementHaut";
+      break;
+    case "imageBas" :
+      idInit = "choixBas";
+      idDest = "magasinVetementBas";
+      break;
+    case "imageChaussure" :
+      idInit = "choixChaussure";
+      idDest = "magasinChaussure";
+      break;
+    default :
+      console.log("Error id image");
+      break;
+  }
+  document.getElementById(idDest).appendChild(nvImage);
+  document.getElementById(idInit).removeChild(image);
+  ajouterOnClick(idDest, "ajouter(this)");
+}
